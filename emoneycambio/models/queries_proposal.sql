a3951a5429d11901cbf4382c8b5ae1e990befc1e
jeffersonnunesfonseca <jeffersonnunesfonseca@gmail.com>
2023-04-24 00:47:42 -0300

    queire

--- emoneycambio/models/queries_proposal.sql
+++ emoneycambio/models/queries_proposal.sql
@@ -15,13 +15,28 @@ SELECT
     if(ep.phone_is_whatsapp=1,'whatsapp','sem_whatsapp') as whatsapp,
     if(ep.delivery=1,'delevery','sem_delivery') as delivery,
     ep.ip as ip_cliente,
-    ep.user_agent 
-    
+    ep.user_agent,
+	CASE
+		WHEN ep.reason = 'pf-reason-1-send' THEN  ' Minha própria conta no exterior'
+		WHEN ep.reason = 'pf-reason-2-send' THEN  'Familiar ou amigo'
+		WHEN ep.reason = 'pf-reason-3-send' THEN  'Pagamento de um produto'
+		WHEN ep.reason = 'pf-reason-4-send' THEN  'Pagamento de um serviço'
+		WHEN ep.reason = 'pf-reason-5-send' THEN  'Investimento'
+		WHEN ep.reason = 'pf-reason-1-receive' THEN  'Minha própria conta no brasil'
+		WHEN ep.reason = 'pf-reason-2-receive' THEN  'Familiar ou amigo'
+		WHEN ep.reason = 'pf-reason-3-receive' THEN  'Recebimento por produto vendido'
+		WHEN ep.reason = 'pf-reason-4-receive' THEN  'Recebimento de serviços prestados'
+		WHEN ep.reason = 'pj-reason-1-send' THEN  'Conta  da empresa no exterior'
+		WHEN ep.reason = 'pj-reason-2-send' THEN  'Pagamento de um produto'
+		WHEN ep.reason = 'pj-reason-3-send' THEN  'Pagamento de um serviço'
+		WHEN ep.reason = 'pj-reason-4-send' THEN  'Investimento'
+		WHEN ep.reason = 'pj-reason-1-receive' THEN  'Conta da empresa no brasil'
+		WHEN ep.reason = 'pj-reason-2-receive' THEN  'Recebimento por produto vendido'
+		WHEN ep.reason = 'pj-reason-3-receive' THEN  'Recebimento de serviços prestados'
+	ELSE "-"
+	END as motivo
 FROM exchange_proposal ep
 inner join company_branch cb on ep.company_branch_id = cb.id
 inner join company c on c.id = cb.company_id
-order by ep.created_at desc
-;
+order by ep.created_at desc;
 
-
-truncate exchange_proposal;
